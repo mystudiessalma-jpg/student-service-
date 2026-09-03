5a8c16753d4c5dcc9e9b57460cab178b43efd955
NM-sree <sreemamatha68@gmail.com>
2026-09-03 16:29:06 +0530

    Added Students API

--- src/main/java/com/salma/StudentService/controller/TestController.java
+++ src/main/java/com/salma/StudentService/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.salma.StudentService.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(  "'test")
+@RequestMapping("/test")
 @CrossOrigin("*")
 public class TestController {
-    @GetMapping("/hello")
-    public ResponseEntity<> hello(){
-        retrun new ResponseEntity("hello",HttpStatus.OK);
+    @GetMapping("/hi")
+    public ResponseEntity<?> hi() {
+        return new ResponseEntity<>("hi", HttpStatus.OK);
     }
+
+    @GetMapping("/student")
+    public ResponseEntity<?> getStudents();
+
+    Map<String, Integer> students = new HashMap<>();
+    students.put("salma,21");
+    students.put("sabiha,22");
+    students,put("suhana,20");
+    students,put("sabbu,23");
+    return new ResponseEntity<>(students,Httpstatus,)
+
 }
